4d43a29c9073db96fe70f2745542851ad94418f3
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-21 03:33:29 +0530

    added:initial commit

---
+++ src/com/bridgelabz/logicalprogram/LgicalPrograms.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.logicalprogram;
+
+public class LgicalPrograms {
+    public static void main(String[] args) {
+        System.out.println("welcome to logical program");
+    }
+}
